c7a5883383de62f723274c183d8baa308b51c01b
V <scriptdy@proton.me>
2023-02-15 22:48:45 +0000

    aula 2

--- app.py
+++ app.py
@@ -1,24 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, session
 
-app = Flask(__name__) 
+app = Flask(__name__)
+SECRET_KEY = "pudim"
+app.config.from_object(__name__)
 
-# POST MOCK
+# POSTS MOCK
 posts = [
     {
         "titulo": "Post 1",
-        "texto": "Meu primeiro post"
+        "texto": "Meu primeiro Post"
     },
     {
         "titulo": "Post 2",
-        "texto": "Segundo Post"
+        "texto": "Olha eu aqui de novo"
     },
-        {
+       {
         "titulo": "Post 3",
         "texto": "Novo Post"
-    },
+    }
 ]
+# USER MOCKS
+USERNAME = "admin"
+PASSWORD = "admin"
 
 @app.route('/')
 def exibir_entradas():
-
     return render_template("exibir_entradas.html", entradas=posts)
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    erro = ""
+    if request.method == "POST":
+        if request.form['username'] == USERNAME and request.form['password'] == PASSWORD:
+            session['logado'] = True
+            return redirect(url_for('exibir_entradas'))
+        erro = "Usuário ou senha inválidos"
+    return render_template("login.html", erro=erro)
